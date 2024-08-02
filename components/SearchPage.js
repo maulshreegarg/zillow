765af70472077d711608e0af8aeef39482ddefd8
maulshreegarg <98210535+maulshreegarg@users.noreply.github.com>
2024-08-02 20:23:48 +0530

    a

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -1,10 +1,13 @@
 import classes from "./search.module.css";
+
 export default function Search()
 {
+    
     return (
         <slug className={classes.slug}>
-          <div>
-      <input className={classes.styledInput} type="text" placeholder="Address, neighborhood, city, ZIP" />
+            <hr/>
+          <div className={classes.styledInput}>
+      <input  type="text" placeholder="Address, neighborhood, city, ZIP" />
     </div>
         </slug>
     );
