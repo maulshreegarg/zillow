cdfae8c6f105c9f2c8302d05e3a4a6038c422d78
maulshreegarg <98210535+maulshreegarg@users.noreply.github.com>
2024-08-02 15:45:15 +0530

    new section

---
+++ components/SearchPage.js
@@ -0,0 +1,9 @@
+import classes from "./search.module.css";
+export default function Search()
+{
+    return (
+        <slug className={classes.slug}>
+            <p>hello mate </p>
+        </slug>
+    );
+}
