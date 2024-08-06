65a0466167280c9bbd0e06b16c026d814d6e445b
maulshreegarg <98210535+maulshreegarg@users.noreply.github.com>
2024-08-06 10:31:56 +0530

    w

--- app/page.js
+++ app/page.js
@@ -1,21 +1,13 @@
-import Image from "next/image";
 import Header from "@/components/headerPage";
 import Search from "@/components/SearchPage";
+import signUp1 from "@/components/signsection";
 export default function Home() {
   return (
     <>
      <Header/>
      <Search/>
-   <main> 
-    <div class="signupsec">
-      <p>
-        Get home recommendation
-      </p>
-      <p>
-      Sign in for a more personalized experience.
-      </p>
-    </div>
-  </main>
+     <signUp1/>
   </>
+
   );
 }
---
+++ components/signsection.js
@@ -0,0 +1,9 @@
+export default function signUp1(){
+    return (
+        <signUp1>
+          <p>Get home recommendation</p>
+          <p>Sign in for a more personalized experience.</p>
+        </signUp1>
+
+    );
+}
